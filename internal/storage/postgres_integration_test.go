c650f6d48041b492c8cace65b9c9628db14e8544
Rod Boev <rod.boev@gmail.com>
2026-09-22 18:36:41 -0400

    test(storage): justify the multiplayer sync pacing sleep

--- internal/storage/postgres_integration_test.go
+++ internal/storage/postgres_integration_test.go
@@ -1322,7 +1322,7 @@ func runConcurrentReviewsAndSync(db *DB, repoID int64, worker *SyncWorker, prefi
 					errs <- fmt.Errorf("%s sync at job %d: %w", author, i, err)
 				}
 			}
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(10 * time.Millisecond) //nolint:kennlint // spaces writes so both machines' PostgreSQL syncs interleave
 		}
 	}()
 }
